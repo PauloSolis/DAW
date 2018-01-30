eda3b30289a0239969b6f84f943d936360569112
Paulo Solis <A01207593@itesm.mx>
2018-01-29 23:52:53 -0600

    subir lab6

---
+++ lab6/Script.js
@@ -0,0 +1,45 @@
+var padre = document.getElementById("Father1");
+var hijo = document.getElementById("pregunta1");
+
+padre.addEventListener("click", function() {
+
+    padre.style.backgroundColor = "black";
+    padre.style.color   = "white";
+}, true);
+
+hijo.addEventListener("click", function () {
+    hijo.style.backgroundColor="red";
+    hijo.style.color= "gray";
+}, true);
+
+var padre2 = document.getElementById("Father2");
+    padre2.addEventListener("mouseenter",function () {
+    padre2.style.color="blue";
+});
+
+var padre2 = document.getElementById("Father2");
+    padre2.addEventListener("mouseleave",function () {
+    padre2.style.color="black";
+});
+
+var padre3 = document.getElementById("Father3");
+padre3.addEventListener("load", function () {
+    console.log("hola");
+    setInterval(function(){
+        if (padre3.style.visibility === "hidden") {
+            padre3.style.visibility = "visible";
+        } else {
+            padre3.style.visibility = "hidden";
+        }
+
+
+    }, 300);
+
+
+
+
+
+});
+
+
+
