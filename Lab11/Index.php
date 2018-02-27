299e81ef0bead1adbeab9cd9fe83ee33080b5e38
Paulo Solis <A01207593@itesm.mx>
2018-02-26 23:27:32 -0600

    subir completo

--- Lab11/Index.php
+++ Lab11/Index.php
@@ -11,24 +11,7 @@
     <article class="grid-container">
         <div class="grid-x align-center">
             <div class="cell medium-8">
-                <h3 id="Formulario"> Formularios</h3>
-                <form>
-                    <p>
-                        <label>Hora y fecha:
-                            <input type="datetime-local">
-                        </label>
-                    </p>
-                    <p>
-                        <label>Correo:
-                            <input placeholder="tuCorreo@mail.com" required="" type="email">
-                        </label>
-                    </p>
-                    <p>
-                        <label>Contraseña:
-                            <input placeholder="Pass" required="" type="password">
-                        </label>
-                    </p>
-                </form>
+                <?php require 'Professional_Development_Workshop_Registration_Form_80568782744874_source.html';?>
             </div>
             <br>
             <br>
@@ -36,11 +19,19 @@
             <div>
                 <h2 id="Me">PHP</h2><br>
 
-                <form action="PHP.php" method="post"    >
+                <form action="PHP.php" method="post">
                     Ingresa Número <input type="text" name="Hola"><br>
                     <input type="submit">
                 </form>
             </div>
+            <h3>preguntas</h3>
+            <p>¿Por qué es una buena práctica separar el controlador de la vista?. <br> para tener una mejor organización y sea
+            más fácil de ver.</p>
+            <p>Aparte de los arreglos $_POST y $_GET, ¿qué otros arreglos están predefinidos en php y cuál es su función?
+                Explora las funciones de php, y describe 2 que no hayas visto en otro lenguaje y que llamen tu atención.</p>
+               <p> ¿Qué es XSS y cómo se puede prevenir?<br> Es una inyección de datos que pueden modificar toda la página, con los proyectos
+                    OWASP ESAPI
+            </p>
         </div>
 
 </article>
---
+++ Lab11/Lab12.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Paulo
+ * Date: 22/02/2018
+ * Time: 11:07 PM
+ */
+
+require '_footer.html';
+
+?>
